5f48769dfddbe90a18ab8f9cadb0d228d6056cdf
arjun <arjunvuppal@gmail.com>
2019-07-06 19:07:38 -0400

    first commit

---
+++ src/test/java/com/mycompany/app/AppiumTest.java
@@ -0,0 +1,10 @@
+package com.mycompany.app;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void browserAutomation() {
+		System.out.println("Appium Test");
+	}
+}
---
+++ src/test/java/com/mycompany/app/SeleniumTest.java
@@ -0,0 +1,11 @@
+package com.mycompany.app;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+@Test
+public void browserAutomation() {
+	System.out.println("Browser Automation");
+}
+}
